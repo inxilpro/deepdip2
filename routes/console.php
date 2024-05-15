954d3e6c776f8c97aa5e863fcdebaf2a573afe7b
Chris Morrell <inxilpro@users.noreply.github.com>
2024-05-15 07:38:16 -0400

    Every minute

--- routes/console.php
+++ routes/console.php
@@ -4,4 +4,4 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('deepdip:live-heights --announce')->everyTenMinutes();
+Schedule::command('deepdip:live-heights --announce')->everyMinute();
